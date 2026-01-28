ba314f34c98d1db3660a13de5923731dd742e8cf
Gilberto Miralla-Flores <gmiralla@kennesaw.edu>
2026-01-28 16:38:17 -0500

    fix: use wrapped resty client

--- users/list/command.go
+++ users/list/command.go
@@ -2,12 +2,11 @@ package list
 
 import (
     "fmt"
-    r "resty.dev/v3"
     "strconv"
     "strings"
     "rhsmctl/internal/api"
     "rhsmctl/internal/cli"
-    "rhsmctl/internal/resty"
+    "rhsmctl/internal/client"
     "rhsmctl/internal/tty"
     "github.com/alecthomas/kong"
 )
@@ -15,39 +14,25 @@ import (
 func (o *Options) Run(ctx *kong.Context, g *cli.Globals) error {
     var (
         acctId string
-        errRes api.AccountErrorRoot
-        okRes api.Account
-        res *r.Response
-        err error
+        errRes api.AccountError
     )
-    client := resty.New(g)
 
+    client := client.New(g)
+
+    // Use account id if provided, otherwise attempt to fetch it.
     if (o.AccountID != 0) {
         acctId = strconv.Itoa(o.AccountID)
     } else {
-        res, err = client.R().
-            SetDebug(g.Debug).
-            SetError(&errRes).
-            SetResult(&okRes).
-            SetQueryParams(map[string]string{
-                "firstResultIndex": strconv.Itoa(o.Offset),
-                "maxResults": strconv.Itoa(o.Limit),
-            }).
-            Get(g.ApiUrl+"/account/v1/accounts")
-        if (err != nil) {
+        id, err := client.AccountID(); if err != nil {
             return err
         }
-        if (res.IsError()) {
-            return fmt.Errorf("error: %s", strings.ToLower(errRes.Detail))
-        }
-        acctId = okRes.Body[0].ID
+        acctId = id
     }
 
-    res, err = client.R().
+    res, err := client.R().
         SetDebug(g.Debug).
         SetError(&errRes).
         SetPathParams(map[string]string{
-            //"accountId": strconv.Itoa(o.AccountID),
             "accountId": acctId,
         }).
         SetQueryParams(map[string]string{
