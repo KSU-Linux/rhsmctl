2c5ed959310a344ff4bf4fd3fa525163d5c60040
Gilberto Miralla-Flores <gmiralla@kennesaw.edu>
2026-03-03 10:07:17 -0500

    feat: support for 'users status' command

--- users/options.go
+++ users/options.go
@@ -3,9 +3,11 @@ package users
 import (
 	"rhsmctl/users/info"
 	"rhsmctl/users/list"
+	"rhsmctl/users/status"
 )
 
 type Options struct {
     Info info.Options `cmd:"" help:"Get details of a user under the account."`
     List list.Options `cmd:"" help:"List all the users under the account."`
+    Status status.Options `cmd:"" help:"Get current status of a user under the account."`
 }
---
+++ users/status/options.go
@@ -0,0 +1,10 @@
+package status
+
+import (
+)
+
+// Options is the customization options for the users status command.
+type Options struct {
+    AccountID int `help:"Red Hat account ID."`
+    UserID int `arg:"" help:"User account ID."`
+}
